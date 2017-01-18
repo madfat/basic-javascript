461b4e5edb65a69370557a8362f3d969886f5d77
Fathoni <akhmad.fathoni@mitrais.com>
2017-01-18 16:51:30 +0700

    Quiz Interactive not finished

---
+++ 11.InteractiveQuiz/script.js
@@ -0,0 +1,83 @@
+var Questions = [
+  {question:"What is the day after Friday?",
+  choices:["Sunday","Tuesday","Saturday", "Wednesday"],
+  correctAnswer: 2},
+  {question:"What is the day before Friday?",
+  choices:["Sunday","Thursday","Saturday", "Wednesday"],
+  correctAnswer: 1},
+  {question:"What is the day after Monday?",
+  choices:["Sunday","Tuesday","Saturday", "Wednesday"],
+  correctAnswer: 1},
+]
+
+var currentQuestion = 0;
+var correctAnswer = 0;
+var quizOver = false;
+
+$(document).ready(function(){
+  displayCurrentQuestion();
+  $(this).find('.quizMessage').hide();
+  $(this).find('.nextButton').on('click',function(){
+    if (!quizOver){
+      value=$('input[type="radio"]:checked').val();
+      if (value==undefined){
+        $(document).find('.quizMessage').text('Please select an answer');
+        $(document).find('.quizMessage').show();
+      }else{
+        quizMsg.hide();
+        if (value == Questions[currentQuestion].correctAnswer){
+          correctAnswer++;
+        }
+        currentQuestion++;
+        if (currentQuestion < Questions.length){
+          displayCurrentQuestion();
+        }else{
+          displayScore();
+          $(document).find('.nextButon').text('Play again?');
+          quizOver=true
+        }
+      }
+    }else{
+      quizOver=false;
+      $(document).find('.nextButton').text('Next Question');
+      resetQuiz();
+      displayCurrentQuestion();
+      hideScore();
+    }
+  })
+})
+
+function displayCurrentQuestion(){
+  console.log("in displaying current question");
+
+  var question = Questions[currentQuestion].question;
+  //?????
+  //var questionClass = $(document).find('.quizContainer > .question');
+  var questionClass = $(document).find('.quizContainer');
+  var choiceList = $(document).find(".quizContainer > .choiceList");
+  //?????
+  var numChoices = Questions[currentQuestion].choices.length;
+  $(questionClass).text(question);
+  $(choiceList).find("li").remove();
+
+  var choice;
+  for (i=0; i < numChoices; i++){
+    choice = Questions[currentQuestion].choices[i];
+    $('<li><input type="radio" value='+ i +' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+  }
+}
+
+function resetQuiz(){
+  currentQuestion = 0;
+  correctAnswer = 0;
+  hideScore();
+}
+
+function displayScore(){
+  $(document).find(".quizContainer > .result").text("your score :" + correctAnswer);
+  $(document).find(".quizContainer > .result").show;
+}
+
+function hideScore(){
+  $(document).find(".result").hide();
+}
